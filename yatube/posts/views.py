0e0df5ce477f16132271a13d959e29becfe1bcd3
Юрий Новиков <iurij.novickov2016@yandex.ru>
2021-12-08 23:37:46 +0300

    111

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -173,8 +173,9 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    if not Follow.objects.filter(user=request.user, author=author).exists() and \
-        author != request.user:
+    if not Follow.objects.filter(user=request.user,
+                                 author=author).exists() and \
+           author != request.user:
         Follow.objects.create(
             user=request.user,
             author=author)
